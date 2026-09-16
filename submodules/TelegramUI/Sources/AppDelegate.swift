ab09aef7da851a3b790915c883443745f74bb35c
femboypig <harimassaa@gmail.com>
2026-09-16 15:12:53 +0400

    Add ToastSlice and Toaster to available alternate icons in AppDelegate.swift

--- submodules/TelegramUI/Sources/AppDelegate.swift
+++ submodules/TelegramUI/Sources/AppDelegate.swift
@@ -950,6 +950,8 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
             if #available(iOS 10.3, *) {
                 var icons = [
                     PresentationAppIcon(name: "BlueIcon", imageName: "BlueIcon", isDefault: buildConfig.isAppStoreBuild),
+                    PresentationAppIcon(name: "ToastSlice", imageName: "ToastSlice"),
+                    PresentationAppIcon(name: "Toaster", imageName: "Toaster"),
                     PresentationAppIcon(name: "New2", imageName: "New2"),
                     PresentationAppIcon(name: "New1", imageName: "New1"),
                     PresentationAppIcon(name: "BlackIcon", imageName: "BlackIcon"),
