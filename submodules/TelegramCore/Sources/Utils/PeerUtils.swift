be8174f1c668e14636f927a553246670a055073c
femboypig <harimassaa@gmail.com>
2026-09-16 17:03:06 +0400

    Allow bypassing copy protection in PeerUtils.swift isCopyProtectionEnabled

--- submodules/TelegramCore/Sources/Utils/PeerUtils.swift
+++ submodules/TelegramCore/Sources/Utils/PeerUtils.swift
@@ -252,6 +252,9 @@ public extension Peer {
     }
     
     var isCopyProtectionEnabled: Bool {
+        if UserDefaults.standard.bool(forKey: "Toast_allowSavingProtectedContent") {
+            return false
+        }
         switch self {
         case let group as TelegramGroup:
             return group.flags.contains(.copyProtectionEnabled)
