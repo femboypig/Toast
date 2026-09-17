c9662ae6d9256807c0f9a071a330a28741883171
femboypig <harimassaa@gmail.com>
2026-09-17 23:58:20 +0400

    Support avatars in direct messages for animated stickers

--- submodules/TelegramUI/Components/Chat/ChatMessageAnimatedStickerItemNode/Sources/ChatMessageAnimatedStickerItemNode.swift
+++ submodules/TelegramUI/Components/Chat/ChatMessageAnimatedStickerItemNode/Sources/ChatMessageAnimatedStickerItemNode.swift
@@ -921,13 +921,14 @@ public class ChatMessageAnimatedStickerItemNode: ChatMessageItemView {
             switch item.chatLocation {
             case let .peer(peerId):
                 if peerId != item.context.account.peerId {
-                    if peerId.isGroupOrChannel && item.message.author != nil {
+                    let showAvatarsInDirect = UserDefaults.standard.object(forKey: "Toast_showAvatarsInDirectChats") as? Bool ?? true
+                    if (peerId.isGroupOrChannel || showAvatarsInDirect) && (item.message.author != nil || item.message.peers[item.message.id.peerId] != nil) {
                         if let peer = item.message.peers[item.message.id.peerId] as? TelegramChannel, case let .broadcast(info) = peer.info {
                             if info.flags.contains(.messagesShouldHaveProfiles) {
                                 hasAvatar = incoming
                             }
                         } else {
-                            hasAvatar = true
+                            hasAvatar = incoming
                         }
                     }
                 } else if incoming {
