800dc3bd6056a4f434c36e02791247ff6bf7a1cb
femboypig <harimassaa@gmail.com>
2026-09-18 00:10:06 +0400

    Filter secret and hidden chats when decoy mode is active in ChatListNodeEntries

--- submodules/ChatListUI/Sources/Node/ChatListNodeEntries.swift
+++ submodules/ChatListUI/Sources/Node/ChatListNodeEntries.swift
@@ -683,6 +683,20 @@ func chatListNodeEntriesForView(view: EngineChatList, state: ChatListNodeState, 
         if let savedMessagesPeer = savedMessagesPeer, let peerId = peerId, savedMessagesPeer.id == peerId || foundPeerIds.contains(peerId) {
             continue loop
         }
+        if UserDefaults.standard.bool(forKey: "Toast_isDecoyActive") {
+            if let peerId = peerId {
+                if peerId.namespace == Namespaces.Peer.SecretChat {
+                    continue loop
+                }
+                if UserDefaults.standard.bool(forKey: "Toast_decoyChannelsOnly") && (peerId.namespace == Namespaces.Peer.CloudUser || peerId.namespace == Namespaces.Peer.SecretChat) {
+                    continue loop
+                }
+                let hiddenPeerIds = UserDefaults.standard.array(forKey: "Toast_decoyHiddenPeerIds") as? [Int64] ?? []
+                if hiddenPeerIds.contains(peerId.toInt64()) {
+                    continue loop
+                }
+            }
+        }
         if let peerId = peerId, state.pendingRemovalItemIds.contains(ChatListNodeState.ItemId(peerId: peerId, threadId: threadId)) {
             continue loop
         }
