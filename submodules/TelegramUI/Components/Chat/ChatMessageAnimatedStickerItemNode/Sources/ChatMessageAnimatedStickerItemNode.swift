1389aadad3f037c953b935d7e7b81d56348552f4
femboypig <harimassaa@gmail.com>
2026-09-18 13:37:32 +0400

    Revert direct message avatars in ChatMessageAnimatedStickerItemNode

--- submodules/TelegramUI/Components/Chat/ChatMessageAnimatedStickerItemNode/Sources/ChatMessageAnimatedStickerItemNode.swift
+++ submodules/TelegramUI/Components/Chat/ChatMessageAnimatedStickerItemNode/Sources/ChatMessageAnimatedStickerItemNode.swift
@@ -921,14 +921,13 @@ public class ChatMessageAnimatedStickerItemNode: ChatMessageItemView {
             switch item.chatLocation {
             case let .peer(peerId):
                 if peerId != item.context.account.peerId {
-                    let showAvatarsInDirect = UserDefaults.standard.object(forKey: "Toast_showAvatarsInDirectChats") as? Bool ?? true
-                    if (peerId.isGroupOrChannel || showAvatarsInDirect) && (item.message.author != nil || item.message.peers[item.message.id.peerId] != nil) {
+                    if peerId.isGroupOrChannel && item.message.author != nil {
                         if let peer = item.message.peers[item.message.id.peerId] as? TelegramChannel, case let .broadcast(info) = peer.info {
                             if info.flags.contains(.messagesShouldHaveProfiles) {
                                 hasAvatar = incoming
                             }
                         } else {
-                            hasAvatar = incoming
+                            hasAvatar = true
                         }
                     }
                 } else if incoming {
