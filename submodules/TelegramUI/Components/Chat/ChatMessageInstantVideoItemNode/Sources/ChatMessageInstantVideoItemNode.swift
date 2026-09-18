87abeb0e51c18dfb1dccd0ba7e0102ac516fbcae
femboypig <harimassaa@gmail.com>
2026-09-18 13:36:24 +0400

    Revert direct message avatars in ChatMessageInstantVideoItemNode

--- submodules/TelegramUI/Components/Chat/ChatMessageInstantVideoItemNode/Sources/ChatMessageInstantVideoItemNode.swift
+++ submodules/TelegramUI/Components/Chat/ChatMessageInstantVideoItemNode/Sources/ChatMessageInstantVideoItemNode.swift
@@ -308,8 +308,7 @@ public class ChatMessageInstantVideoItemNode: ChatMessageItemView, ASGestureReco
             
             do {
                 if messagePeerId != item.context.account.peerId {
-                    let showAvatarsInDirect = UserDefaults.standard.object(forKey: "Toast_showAvatarsInDirectChats") as? Bool ?? true
-                    if (messagePeerId.isGroupOrChannel || showAvatarsInDirect) && (item.message.author != nil || item.message.peers[item.message.id.peerId] != nil) {
+                    if messagePeerId.isGroupOrChannel && item.message.author != nil {
                         var isBroadcastChannel = false
                         if let peer = item.message.peers[item.message.id.peerId] as? TelegramChannel, case .broadcast = peer.info {
                             isBroadcastChannel = true
@@ -320,7 +319,7 @@ public class ChatMessageInstantVideoItemNode: ChatMessageItemView, ASGestureReco
                         }
                         
                         if !isBroadcastChannel {
-                            hasAvatar = incoming
+                            hasAvatar = true
                         } else if case .customChatContents = item.chatLocation {
                             hasAvatar = false
                         }
